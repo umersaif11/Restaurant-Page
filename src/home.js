332bc26186e799d13c329b09d5b7ff41ce084b75
Muhammad Umar <muhammadumar.tudortmund@gmail.com>
2025-06-24 15:47:11 +0500

    Make all divs inside both child containers inside hourAndInfoDiv

--- src/home.js
+++ src/home.js
@@ -39,8 +39,49 @@ function home(){
     const hourDiv = document.createElement("div");
     hourDiv.setAttribute("id", "hourDiv");
 
-    const infoDiv = document.createElement("id", "infoDiv");
+    const hourHeading = document.createElement("div");
+    hourHeading.setAttribute("id", "hourHeading");
+    hourHeading.textContent = "Our Hours";
+    const monToFriDiv = document.createElement("div");
+    monToFriDiv.setAttribute("id", "monToFriDiv");
+    monToFriDiv.textContent = "Monday – Friday: 11:00 AM – 10:00 PM";
+    const satDiv = document.createElement("div");
+    satDiv.setAttribute("id", "satDiv");
+    satDiv.textContent = "Saturday: 12:00 PM – 11:00 PM";
+    const sunDiv = document.createElement("div");
+    sunDiv.setAttribute("id", "sunDiv");
+    sunDiv.textContent = "Sunday: 12:00 PM – 9:00 PM";
+
+    hourDiv.appendChild(hourHeading);
+    hourDiv.appendChild(monToFriDiv);
+    hourDiv.appendChild(satDiv);
+    hourDiv.appendChild(sunDiv);
+
+    const infoDiv = document.createElement("div");
     infoDiv.setAttribute("id", "infoDiv");
+
+    const infoHeadingDiv = document.createElement("div");
+    infoHeadingDiv.setAttribute("id", "infoHeadingDiv");
+    infoHeadingDiv.textContent = "Information";
+    const infoAddressDiv = document.createElement("div");
+    infoAddressDiv.setAttribute("id", "infoAddressDiv");
+    infoAddressDiv.textContent = "123 Flavor Street, Foodville, FV 45678";
+    const infoPhoneDiv = document.createElement("div");
+    infoPhoneDiv.setAttribute("id", "infoPhoneDiv");
+    infoPhoneDiv.textContent = "Phone: (555) 123-4567";
+    const infoEmailDiv = document.createElement("div");
+    infoEmailDiv.setAttribute("id", "infoEmailDiv");
+    infoEmailDiv.textContent = "Email: contact@ourrestaurant.com";
+    const infoReserveDiv = document.createElement("div");
+    infoReserveDiv.setAttribute("id", "infoReserveDiv");
+    infoReserveDiv.textContent = "Reservations: (555) 765-4321";
+
+    infoDiv.appendChild(infoHeadingDiv);
+    infoDiv.appendChild(infoAddressDiv);
+    infoDiv.appendChild(infoPhoneDiv);
+    infoDiv.appendChild(infoEmailDiv);
+    infoDiv.appendChild(infoReserveDiv);
+
 
     hourAndInfoDiv.appendChild(hourDiv);
     hourAndInfoDiv.appendChild(infoDiv);
