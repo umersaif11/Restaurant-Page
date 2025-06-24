48522b4aad7dae6be521bae1605a5744e044a39e
Muhammad Umar <muhammadumar.tudortmund@gmail.com>
2025-06-24 12:09:41 +0500

    Now make introduction and welcome visiting customer in the second div i.e restaurantIntroDiv by using text

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
-import bgimage from "./bgimg.png";
+import backimage from "./bgimg.png";
 function home(){
     const contentDiv = document.getElementById("content");
     
@@ -7,10 +7,38 @@ function home(){
 
     const bgImage = document.createElement("img");
     bgImage.setAttribute("id", "bg-image");
-    bgImage.setAttribute("src", bgimage);
+    bgImage.setAttribute("src", backimage);
     introDiv.appendChild(bgImage);
 
+    const restaurantIntroDiv = document.createElement("div");
+    restaurantIntroDiv.setAttribute("id", "restaurantIntroDiv");
+
+    const welcomeDiv = document.createElement("div");
+    welcomeDiv.setAttribute("id", "welcomeDiv");
+    welcomeDiv.textContent = "Welcome to Urban Bites'";
+
+    const paraDiv = document.createElement("div");
+    paraDiv.setAttribute("id", "paraDiv");
+    paraDiv.textContent = `Experience the best flavors and 
+    freshest ingredients in a warm, friendly atmosphere. Whether 
+    you’re craving a classic burger or our famous spicy wings, we’ve 
+    got something for everyone. `
+
+    const paraTwoDiv = document.createElement("div");
+    paraTwoDiv.setAttribute("id", "paraTwoDiv");
+    paraTwoDiv.textContent = `Visit us today and enjoy great food, great 
+    company, and great memories! `;
+
+    restaurantIntroDiv.appendChild(welcomeDiv);
+    restaurantIntroDiv.appendChild(paraDiv);
+    restaurantIntroDiv.appendChild(paraTwoDiv);
+
+    const hourDiv = document.createElement("div");
+    hourDiv.setAttribute("id", "hourDiv");
+
     contentDiv.appendChild(introDiv);
+    contentDiv.appendChild(restaurantIntroDiv);
+    contentDiv.appendChild(hourDiv);
 }
 
 
