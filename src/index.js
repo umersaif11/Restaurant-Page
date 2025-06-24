ddd1345ccb31be46d4e03c3f368ce8fca710ccb5
Muhammad Umar <muhammadumar.tudortmund@gmail.com>
2025-06-24 16:23:29 +0500

    Write the tab switching logic in index.js file while also importing functions from home and menu modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import "./styles.css";
 import {home} from "./home.js";
+import {menu} from "./menu.js";
+
 
 home();
+const headClick = document.getElementById("head");
+const contentDiv = document.getElementById("content");
+headClick.addEventListener("click", (e) => {
+    console.log(e.target.id);
+    if(e.target.id === "home"){
+        contentDiv.innerHTML = "";
+        home();
+    }
+    if(e.target.id === "menu"){
+        contentDiv.innerHTML = "";
+        menu();
+    }
+});
 console.log("Hello Odinite!");
