15df384f5ab0da7793af73581bdb622b48d4e940
Muhammad Umar <muhammadumar.tudortmund@gmail.com>
2025-06-25 12:07:01 +0500

    Make five divs inside the burgersDiv

--- src/menu.js
+++ src/menu.js
@@ -4,6 +4,24 @@ function menu(){
     const burgerMenu = document.createElement("div");
     burgerMenu.setAttribute("id", "burgerMenu");
 
+    const menuHeading = document.createElement("div");
+    menuHeading.setAttribute("id", "menuHeading");
+    menuHeading.textContent = "Our Menu";
+    const burgerHeading = document.createElement("div");
+    burgerHeading.setAttribute("id", "burgerHeading");
+    const cheeseBurger = document.createElement("div");
+    cheeseBurger.setAttribute("id", "cheeseBurger");
+    const baconBurger = document.createElement("div");
+    baconBurger.setAttribute("id", "baconBurger");
+    const spicyBurger = document.createElement("div");
+    spicyBurger.setAttribute("id", "spicyBurger");
+
+    burgerMenu.appendChild(menuHeading);
+    burgerMenu.appendChild(burgerHeading);
+    burgerMenu.appendChild(cheeseBurger);
+    burgerMenu.appendChild(baconBurger);
+    burgerMenu.appendChild(spicyBurger);
+
     const friesMenu = document.createElement("div");
     friesMenu.setAttribute("id", "friesMenu");
 
