91c7aeeed7a006a05ca5b684f25fb6cc6da07e29
Muhammad Umar <muhammadumar.tudortmund@gmail.com>
2025-06-25 15:37:17 +0500

    Make the third div for cheeseburger and within it three more div childs to make a card

--- src/menu.js
+++ src/menu.js
@@ -10,10 +10,28 @@ function menu(){
     const burgerHeading = document.createElement("div");
     burgerHeading.setAttribute("id", "burgerHeading");
     burgerHeading.textContent = "Burgers";
+
     const cheeseBurger = document.createElement("div");
     cheeseBurger.setAttribute("id", "cheeseBurger");
+
+    const cheeseBurgerHeading = document.createElement("div");
+    cheeseBurgerHeading.setAttribute("id", "cheeseBurgerHeading");
+    cheeseBurgerHeading.textContent = "Classic CheeseBurger";
+    const cheeseBurgerIngredients = document.createElement("div");
+    cheeseBurgerIngredients.setAttribute("id", "cheeseBurgerIngredients");
+    cheeseBurgerIngredients.textContent = `Juicy beef patty, cheddar cheese,
+    lettuce, tomato, onion, and house sauce on a toasted bun.`;
+    const cheeseBurgerPrice = document.createElement("div");
+    cheeseBurgerPrice.setAttribute("id", "cheeseBurgerPrice");
+    cheeseBurgerPrice.textContent = "$8.99";
+
+    cheeseBurger.appendChild(cheeseBurgerHeading);
+    cheeseBurger.appendChild(cheeseBurgerIngredients);
+    cheeseBurger.appendChild(cheeseBurgerPrice);
+
     const baconBurger = document.createElement("div");
     baconBurger.setAttribute("id", "baconBurger");
+
     const spicyBurger = document.createElement("div");
     spicyBurger.setAttribute("id", "spicyBurger");
 
