69ed21ac6694c8cb3af1f1527c326eb96e7aa3f9
Muhammad Umar <muhammadumar.tudortmund@gmail.com>
2025-06-26 14:32:28 +0500

    Make cards of french fries and loaded fries complete

--- src/menu.js
+++ src/menu.js
@@ -81,11 +81,56 @@ function menu(){
     const frenchFries = document.createElement("div");
     frenchFries.setAttribute("id", "frenchFries");
 
+    const frenchFriesHeading = document.createElement("div");
+    frenchFriesHeading.setAttribute("id", "frenchFriesHeading");
+    frenchFriesHeading.textContent = "Classic French Fries";
+    const frenchFriesIngredients = document.createElement("div");
+    frenchFriesIngredients.setAttribute("id", "frenchFriesIngredients");
+    frenchFriesIngredients.textContent = `Golden, crispy fries served 
+    with ketchup or house-made aioli.`;
+    const frenchFriesPrice = document.createElement("div");
+    frenchFriesPrice.setAttribute("id", "frenchFriesPrice");
+    frenchFriesPrice.textContent = "$3.49";
+
+    frenchFries.appendChild(frenchFriesHeading);
+    frenchFries.appendChild(frenchFriesIngredients);
+    frenchFries.appendChild(frenchFriesPrice);
+
     const loadedFries = document.createElement("div");
     loadedFries.setAttribute("id", "loadedFries");
 
+    const loadedFriesHeading = document.createElement("div");
+    loadedFriesHeading.setAttribute("id", "loadedFriesHeading");
+    loadedFriesHeading.textContent = "Loaded Fries";
+    const loadedFriesIngredients = document.createElement("div");
+    loadedFriesIngredients.setAttribute("id", "loadedFriesIngredients");
+    loadedFriesIngredients.textContent = `Fries topped with cheese, bacon
+     bits, and ranch drizzle.`;
+    const loadedFriesPrice = document.createElement("div");
+    loadedFriesPrice.setAttribute("id", "loadedFriesPrice");
+    loadedFriesPrice.textContent = "$5.99";
+
+    loadedFries.appendChild(loadedFriesHeading);
+    loadedFries.appendChild(loadedFriesIngredients);
+    loadedFries.appendChild(loadedFriesPrice);
+
     const spicyFries = document.createElement("div");
     spicyFries.setAttribute("id", "spicyFries");
+
+    const spicyFriesHeading = document.createElement("div");
+    spicyFriesHeading.setAttribute("id", "spicyFriesHeading");
+    spicyFriesHeading.textContent = "Spicy Cajun Fries";
+    const spicyFriesIngredients = document.createElement("div");
+    spicyFriesIngredients.setAttribute("id", "spicyFriesIngredients");
+    spicyFriesIngredients.textContent = `Fries tossed in our signature 
+    Cajun seasoning blend.`;
+    const spicyFriesPrice = document.createElement("div");
+    spicyFriesPrice.setAttribute("id", "spicyFriesPrice");
+    spicyFriesPrice.textContent = "$4.49";
+
+    spicyFries.appendChild(spicyFriesHeading);
+    spicyFries.appendChild(spicyFriesIngredients);
+    spicyFries.appendChild(spicyFriesPrice);
 
     friesMenu.appendChild(friesHeading);
     friesMenu.appendChild(frenchFries);
